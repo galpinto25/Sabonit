33b675c86aca05020a0f888b553375cf182a48ad
galium31 <gal.pinto@mail.huji.ac.il>
2020-01-19 15:57:27 +0200

    added user login activity (by a Google user)

--- app/src/main/java/com/example/sabonit/CategoriesActivity.java
+++ app/src/main/java/com/example/sabonit/CategoriesActivity.java
@@ -1,5 +1,6 @@
 package com.example.sabonit;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,19 +11,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class CategoriesActivity extends AppCompatActivity {
 
     // todo: All the next code suppose to move to the relevant place (another activity)
-    //this is our db that conatins all the infromation of the app
+    //this is our db that contains all the information of the app
     private FirebaseFirestore db;
 
     @Override
@@ -73,4 +80,5 @@ public class CategoriesActivity extends AppCompatActivity {
                     }
                 });
     }
+
 }
