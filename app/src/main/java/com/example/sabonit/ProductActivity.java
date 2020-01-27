4edf88f2ee9ec12f9efcf8bc579d6692539aec90
galium31 <gal.pinto@mail.huji.ac.il>
2020-01-27 14:08:14 +0200

    added option radio buttons to choose scents

--- app/src/main/java/com/example/sabonit/ProductActivity.java
+++ app/src/main/java/com/example/sabonit/ProductActivity.java
@@ -14,8 +14,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -46,6 +48,8 @@ public class ProductActivity extends AppCompatActivity {
     private double currentProductLiters;
     private int initalProgress;
     private SeekBar literSeekBar;
+    private RadioGroup radioScentGroup;
+    private int bottleColor = R.style.ColorRoses;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -58,6 +62,13 @@ public class ProductActivity extends AppCompatActivity {
         litersTitle = findViewById(R.id.liters);
         productImage = findViewById(R.id.product_image);
         literSeekBar = findViewById(R.id.liter_seek_bar);
+        radioScentGroup = findViewById(R.id.scent_options);
+        radioScentGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                changeScent();
+            }
+        });
         Bundle bundle = getIntent().getExtras();
         db = FirebaseFirestore.getInstance();
         products = new ArrayList<>();
@@ -76,7 +87,7 @@ public class ProductActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 double numLiters = progress / 10.0;
                 litersTitle.setText(String.valueOf(numLiters) + "L");
-                fillBottle(numLiters, R.style.ColorRoses);
+                fillBottle(numLiters, bottleColor);
             }
 
             @Override
@@ -215,6 +226,24 @@ public class ProductActivity extends AppCompatActivity {
 
     // todo needs to be implemented
     public void setSeekBar(double liters) {
+    }
+
+    public void changeScent() {
+        // get selected radio button from radioGroup
+        int selectedId = radioScentGroup.getCheckedRadioButtonId();
+        Product currentProduct = products.get(currentProductIndex);
+        switch (selectedId) {
+            case R.id.radioButton1:
+                bottleColor = R.style.ColorRoses;
+                break;
+            case R.id.radioButton2:
+                bottleColor = R.style.ColorAqua;
+                break;
+            case R.id.radioButton3:
+                bottleColor = R.style.ColorApple;
+                break;
+        }
+        fillBottle(currentProductLiters, bottleColor);
     }
 
 }
