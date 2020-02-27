aa7e4a07482f466dac21567cb01aecbebf74b0d3
eilon.asher <eilon.asher@mail.huji.ac.il>
2020-02-27 14:43:47 +0200

    finished cleaning my parts.

--- app/src/main/java/com/example/sabonit/Utils.java
+++ app/src/main/java/com/example/sabonit/Utils.java
@@ -4,11 +4,11 @@ public class Utils {
     static String CANT_ORDER_EMPTY_CART = "You can't order empty cart";
     static String YOUR_CART_IS_EMPTY = "Your cart is\n empty...\n\uD83D\uDE44";
     static String ORDER_MESSAGE = "order_message";
-    static int[] bodyXMLS = { R.drawable.ic_body_soap_new_2,R.drawable.ic_body_soap_new_3,
-            R.drawable.ic_body_soap_new_4,R.drawable.ic_body_soap_new_5,
-            R.drawable.ic_body_soap_new_6,R.drawable.ic_body_soap_new_7,
-            R.drawable.ic_body_soap_new_8,R.drawable.ic_body_soap_new_9,
-            R.drawable.ic_body_soap_new_10,R.drawable.ic_body_soap_new_11};
+    static int[] bodyXMLS = { R.drawable.ic_body_soap_new_1,R.drawable.ic_body_soap_new_2,
+            R.drawable.ic_body_soap_new_3,R.drawable.ic_body_soap_new_4,
+            R.drawable.ic_body_soap_new_5,R.drawable.ic_body_soap_new_6,
+            R.drawable.ic_body_soap_new_7,R.drawable.ic_body_soap_new_8,
+            R.drawable.ic_body_soap_new_9,R.drawable.ic_body_soap_new_10};
     static int[] handsXMLS = { R.drawable.ic_hand_soap_new_1, R.drawable.ic_hand_soap_new_2,
             R.drawable.ic_hand_soap_new_3,R.drawable.ic_hand_soap_new_4,
             R.drawable.ic_hand_soap_new_5,R.drawable.ic_hand_soap_new_6,
